a9f1cfe95d8f51318da7c9cf6122e55fca9cafb5
Theo <ronna88@gmail.com>
2025-03-01 15:28:02 -0300

    Add initial components and utilities for Todo application

---
+++ app/todo/page.tsx
@@ -0,0 +1,20 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/app/_components/ui/card";
+
+const TodoPage = () => {
+  return (
+    <Card>
+      <CardTitle>Todo Page</CardTitle>
+      <CardDescription>This is a todo page</CardDescription>
+      <CardContent>
+        <p>Todo page content</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TodoPage;
